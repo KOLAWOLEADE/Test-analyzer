4064768cd9a24a1f5fed617cdbc93fe5db508c19
kolawole adeyemo <kolawole_adeyemo@yahoo.com>
2022-08-28 14:44:57 +0100

    logic to check for number

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
-// Business Logic
+// To check for empty str
+
+// if (text.length === 0) {
+
+//     return 0
+// }
+
+
 
 function wordCounter(text) {
+    if (text.trim().length === 0) {
+
+        return 0
+    }
     let wordCount = 0;
     const wordArray = text.split(" ");
     wordArray.forEach(function(word) {
+        // logic to check for number
+
+        if (!Number(element))
       wordCount++;
     });
     return wordCount;
