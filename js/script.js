1075cbcd6c1987fdda9b085427b71f7edcbb451d
kolawole adeyemo <kolawole_adeyemo@yahoo.com>
2022-08-28 15:08:25 +0100

    add logic  to check for empty string and spaces

--- js/script.js
+++ js/script.js
@@ -17,8 +17,27 @@ function wordCounter(text) {
     wordArray.forEach(function(word) {
         // logic to check for number
 
-        if (!Number(element))
-      wordCount++;
+        if (!Number(element)){
+      wordCount++;}
+
+    });
+    return wordCount;
+  }
+
+
+
+  // function to check the number of occurences in a text
+
+  function numberOfOccurrencesInText(word, text) {
+if (text.trim.length === 0 ){
+    return 0;
+}
+    const wordArray = text.split(" ");
+    let wordCount = 0;
+    wordArray.forEach(function(element) {
+      if (word === element) {
+        wordCount++
+      }
     });
     return wordCount;
   }
